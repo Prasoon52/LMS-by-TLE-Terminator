34c98e93a50bb6a6d2afc81d6b90e7c4d7e8ef3a
Prasoon52 <lci2024051@iiitl.ac.in>
2026-01-15 18:40:05 +0530

    fixed some db issue

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDb = async () => {
-  try {
-    await mongoose.connect(`${process.env.DB_URL}${process.env.DB_NAME}`);
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.error("❌ DB connection error:", error.message);
-    process.exit(1);
-  }
-};
-
-export default connectDb;
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("DB connected")
+    } catch (error) {
+        console.log("DB error")
+    }
+}
+export default connectDb
